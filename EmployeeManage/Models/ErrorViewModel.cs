d1dc76a1b3dda48dcc340855ed49bbd655a7f655
Marcus Cheong <marcus.cheong@nus.edu.sg>
2025-10-07 10:58:47 +0800

    clean up folders

--- EmployeeManage/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EmployeeManage.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
